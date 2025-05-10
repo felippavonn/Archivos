22c6d58b7b9e8ce42bf440c2bdd8e9b1a60b84b4
felippavonn <167126616+felippavonn@users.noreply.github.com>
2025-05-10 19:03:03 -0300

    Agrego ejercicios

---
+++ Ej2.cpp
@@ -0,0 +1,73 @@
+/* 2) Modificar el programa anterior de manera que se puedan realizar búsquedas por
+Apellido y por nombre a la vez.*/
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+int main()
+{
+    FILE *datos;
+    char nombre[20], apellido[20], buscarNombre[20], buscarApellido[20];
+    int op, DNI, buscarDni;    
+	bool proceder = true;
+	
+    do
+	{
+        printf("Ingrese la opcion que quiera \n");
+        printf("1. Ingresar nuevos datos\n");
+        printf("2. Busqueda mediante nombre y apellido\n");
+        printf("3. Salir del programa\n: ");
+        scanf("%d", &op);
+        switch(op)
+		{
+            case 1:
+            printf("Ingrese nombre: ");
+            scanf("%s", nombre);
+            printf("Ingrese apellido: ");
+            scanf("%s", apellido);
+            printf("Ingrese DNI: ");   
+            scanf("%d", &DNI);
+            datos = fopen("datos.txt", "a+");
+            
+            if(datos == NULL)
+			{
+                printf("No se pudo abrir el archivo\n");
+                return 1;
+            }
+            fprintf(datos, "%s %s %d\n", nombre, apellido, DNI);
+            fclose(datos);
+            break;
+            
+            case 2:
+            printf("Ingrese el apellido que desea buscar: ");
+            scanf("%s", buscarApellido);
+            printf("Ingrese el nombre que desea buscar: ");
+            scanf("%s", buscarNombre);
+
+            datos = fopen("datos.txt", "r");
+
+            if(datos == NULL){
+                printf("No se pudo abrir el archivo\n");
+                return 1;
+            }          
+
+            while(fscanf(datos, "%s %s %d", nombre, apellido, &DNI) != EOF)
+			{
+                if(strcmp(apellido, buscarApellido) == 0 && strcmp(nombre, buscarNombre) == 0)
+				{
+                    printf("Nombre: %s\n", nombre);
+                    printf("Apellido: %s\n", apellido);
+                    printf("DNI: %d\n", DNI);
+                }
+            }
+            fclose(datos);
+            break;
+            
+            case 3:
+                proceder = false;
+            break;
+        }
+    }while(proceder);
+    return 0;
+
+}
